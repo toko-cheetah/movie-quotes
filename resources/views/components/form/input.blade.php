6c9651ea9dffdd10c66acdc3a93f8a1c65e9a995
Tornike Khatiashvili <100285138+toko-cheetah@users.noreply.github.com>
2022-09-02 13:59:46 +0400

    fix(login): input type

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
-@props(['name'])
+@props(['name', 'type'])
 
 <label class="mb-2 uppercase text-lg"
     for="{{ $name }}"
@@ -10,5 +10,6 @@
     class="text-gray-700 text-4xl py-3 px-6 mb-5 rounded-2xl"
     name="{{ $name }}"
     id="{{ $name }}"
+    type="{{ $type }}"
     {{ $attributes(['value' => old($name)]) }}
 >
